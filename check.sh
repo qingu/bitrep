6e2f1ee3345862152b9cf68a1aa40ca8833e2bca
Jiang Qingu <jiangqingu@gmail.com>
2025-12-04 09:27:21 +0800

    feat: add check math funcs

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for x in SIN_ COS_ ASIN ATAN EXP_ LOG_
+for x in SIN_ COS_ ASIN ATAN EXP_ LOG_ ERF_ ERFC_ GAMMA_ LOGGAMMA_
 do
   echo "==> $x <=="
   diff Z$x.b.gpu.dat Z$x.b.cpu.dat
