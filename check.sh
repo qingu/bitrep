54f5097124ab68c722c641fcf469164ed67cf9f2
qingu <jiangqingu@gmail.com>
2025-12-10 09:15:36 +0800

    feat: add binary output and compare

--- check.sh
+++ check.sh
@@ -2,7 +2,16 @@
 
 for x in SIN_ COS_ ASIN ATAN EXP_ LOG_ ERF_ ERFC_ GAMMA_ LOGGAMMA_
 do
-  echo "==> $x <=="
+  echo "==> $x text<=="
   diff -q Z$x.b.gpu.dat Z$x.b.cpu.dat
   diff -q Z$x.b.gpu.dat Z$x.B.gpu.dat
+done
+
+echo "====================================================="
+
+for x in SIN_ COS_ ASIN ATAN EXP_ LOG_ ERF_ ERFC_ GAMMA_ LOGGAMMA_
+do
+  echo "==> $x bin<=="
+  diff -q Z$x.b.gpu.bin Z$x.b.cpu.bin
+  diff -q Z$x.b.gpu.bin Z$x.B.gpu.bin
 done
--- check_across_compilers.sh
+++ check_across_compilers.sh
@@ -4,8 +4,18 @@ dir1=$1
 dir2=$2
 for x in SIN_ COS_ ASIN ATAN EXP_ LOG_ ERF_ ERFC_ GAMMA_ LOGGAMMA_
 do
-  echo "==> $x intrinsic<=="
+  echo "==> $x intrinsic text<=="
   diff -q $dir1/Z$x.i.cpu.dat $dir2/Z$x.i.cpu.dat
-  echo "==> $x BR custom<=="
+  echo "==> $x BR custom text<=="
   diff -q $dir1/Z$x.b.cpu.dat $dir2/Z$x.b.cpu.dat
+done
+
+echo "======================================================"
+
+for x in SIN_ COS_ ASIN ATAN EXP_ LOG_ ERF_ ERFC_ GAMMA_ LOGGAMMA_
+do
+  echo "==> $x intrinsic bin<=="
+  diff -q $dir1/Z$x.i.cpu.bin $dir2/Z$x.i.cpu.bin
+  echo "==> $x BR custom bin<=="
+  diff -q $dir1/Z$x.b.cpu.bin $dir2/Z$x.b.cpu.bin
 done
